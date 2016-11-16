d41cd22b70d0671c60226bc04d04211fa380aa37
Chris <chriscrawf95@gmail.com>
2016-11-16 01:57:17 -0500

    BoardConfigCommon: Fix Cmdline
    Change-Id: I6b8c80774aa9c56e58bec3cd7dfccb95fb15be3d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 vmalloc=400M4
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 vmalloc=400M
 
 WLAN_MODULES:
 	mkdir -p $(KERNEL_MODULES_OUT)/pronto
